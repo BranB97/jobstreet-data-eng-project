feb15d0b585eeb36e5b07d6ea893b96b9df1c802
Brandon <AuralWintermyst@hotmail.com>
2024-07-25 21:22:26 +0800

    Add files via upload

---
+++ job-scraper/std_para.py
@@ -0,0 +1,11 @@
+BASE_URL = "https://www.jobstreet.com.my/"
+GECKODRIVER_PATH = r"C:\Users\Calicomp\geckodriver-v0.34.0-win32\geckodriver.exe"
+KEYWORDS = [
+    "Data Engineer"
+]
+LOG_DIR = r"C:\Users\Calicomp\Documents\Data_Engineering_Projects\Jobstreet_Job_Scraper\log"
+LOG_INFO_PATH = f"{LOG_DIR}\\jobscrap.log"
+LOG_INFO_FILEMODE = "w"
+DATA_DIR = r"C:\Users\Calicomp\Documents\Data_Engineering_Projects\Jobstreet_Job_Scraper\data"
+MAX_PAGE = 23
+FIREFOX_PATH =r"C:\Program Files\Mozilla Firefox\firefox.exe"
