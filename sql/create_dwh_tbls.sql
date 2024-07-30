4218595ea22d8d973e9f01d16f8766d1c9300e13
Brandon <AuralWintermyst@hotmail.com>
2024-07-30 20:24:13 +0800

    Update create_dwh_tbls.sql
    remove analysis_tbl creation

--- sql/create_dwh_tbls.sql
+++ sql/create_dwh_tbls.sql
@@ -48,38 +48,3 @@ CREATE TABLE jobstreet_de_dwh.company_dim_tbl AS
            max_size,
          FROM jobstreet_de_dwh.jobs_stg_tbl
          GROUP BY company_name, company_review, company_benefits, company_size, min_size, max_size;
-
-
-DROP TABLE IF EXISTS jobstreet_de_dwh.jobstreet_fact_tbl;
-CREATE TABLE jobstreet_de_dwh.jobstreet_fact_tbl AS
-SELECT DISTINCT  job_dim_tbl.job_id,
-	       company_dim_tbl.company_id,
-	       job_type_dim_tbl.job_type_id,
-		   job_class_dim_tbl.job_class_id,
-		   industry_dim_tbl.industry_id,
-		   location_dim_tbl.location_id,
-		   jobs_stg_tbl.min_salary,
-           jobs_stg_tbl.max_salary,
-		   jobs_stg_tbl.min_size,
-		   jobs_stg_tbl.max_size
-    FROM jobstreet_de_dwh.jobs_stg_tbl jobs_stg_tbl
-	
-	INNER JOIN jobstreet_de_dwh.job_type_dim_tbl job_type_dim_tbl 
-	ON job_type_dim_tbl.job_type = jobs_stg_tbl.job_type
-	
-	INNER JOIN jobstreet_de_dwh.job_class_dim_tbl job_class_dim_tbl 
-	ON job_class_dim_tbl.job_classification = jobs_stg_tbl.job_classification
-	
-	INNER JOIN jobstreet_de_dwh.industry_dim_tbl industry_dim_tbl 
-	ON industry_dim_tbl.company_industry = jobs_stg_tbl.company_industry
-	
-	INNER JOIN jobstreet_de_dwh.location_dim_tbl location_dim_tbl 
-	ON location_dim_tbl.job_location = jobs_stg_tbl.job_location
-	
-	INNER JOIN jobstreet_de_dwh.company_dim_tbl company_dim_tbl 
-	ON company_dim_tbl.company_name = jobs_stg_tbl.company_name
-
-	INNER JOIN jobstreet_de_dwh.job_dim_tbl job_dim_tbl 
-	ON job_dim_tbl.job_description = jobs_stg_tbl.job_description
-  AND job_dim_tbl.company_name = jobs_stg_tbl.company_name
-	;
